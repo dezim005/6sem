c4f06a99c935a9f342f9e17c62ae8f12423bf31b
Andre <andre.lopes.1521271@sga.pucminas.br>
2026-09-24 19:30:40 -0300

    vai

--- src/backend/lib/mail.ts
+++ src/backend/lib/mail.ts
@@ -1,4 +1,4 @@
 import { Resend } from 'resend';
 
 // Inicializa a API do Resend usando a chave de ambiente 
-export const resend = new Resend(process.env.RESEND\_API\_KEY);
+export const resend = new Resend(process.env.RESEND_API_KEY);
